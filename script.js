3212e8bbf5542e99880bdeae8b0dd93a5eb7562a
Naushi-Ali <49521116+naushi-Ali@users.noreply.github.com>
2019-07-20 05:23:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,189 @@
+let intelligenceLevels=["smart","intelligent","awesome","dumb","attractive","einstine"]
+
+function showResult();{
+
+let name = document.querySelector("input").value;
+let intelligence;
+if(name==false) {
+
+    alert("please enter a name");
+
+}
+else if (name,charAt(0).toLowerCase()=="a") {
+        intelligence =intelligenceLevels(1);
+    alert (intelligence);
+ }
+
+ else 
+     if (name,charAt(0).toLowerCase()=="b") {
+        intelligence =intelligenceLevels(2);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="c") {
+        intelligence =intelligenceLevels(3);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="d") {
+        intelligence =intelligenceLevels(4);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="e") {
+        intelligence =intelligenceLevels(5);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="f") {
+        intelligence =intelligenceLevels(6);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="g") {
+        intelligence =intelligenceLevels(7);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="h") {
+        intelligence =intelligenceLevels(6);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="i") {
+        intelligence =intelligenceLevels(1);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="ja") {
+        intelligence =intelligenceLevels(2);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="k") {
+        intelligence =intelligenceLevels(3);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="l") {
+        intelligence =intelligenceLevels(4);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="m") {
+        intelligence =intelligenceLevels(5);
+    alert (intelligence);
+ }
+ else 
+    if (name,charAt(0).toLowerCase()=="n") {
+        intelligence =intelligenceLevels(6);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="a") {
+        intelligence =intelligenceLevels(1);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="o") {
+        intelligence =intelligenceLevels(2);
+    alert (intelligence);
+ }
+
+ else 
+
+    if (name,charAt(0).toLowerCase()=="a") {
+        intelligence =intelligenceLevels(3);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="p") {
+        intelligence =intelligenceLevels(4);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="a") {
+        intelligence =intelligenceLevels(5);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="q") {
+        intelligence =intelligenceLevels(6);
+    alert (intelligence);
+ }
+
+ else
+
+    if (name,charAt(0).toLowerCase()=="r") {
+        intelligence =intelligenceLevels(1);
+    alert (intelligence);
+ }
+ else 
+    if (name,charAt(0).toLowerCase()=="s") {
+        intelligence =intelligenceLevels(6);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="t") {
+        intelligence =intelligenceLevels(2);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="u") {
+        intelligence =intelligenceLevels(6);
+    alert (intelligence);
+ }
+ else 
+    if (name,charAt(0).toLowerCase()=="a") {
+        intelligence =intelligenceLevels(3);
+    alert (intelligence);
+ }
+
+ else 
+
+    if (name,charAt(0).toLowerCase()=="v") {
+        intelligence =intelligenceLevels(4);
+    alert (intelligence);
+ }
+ else 
+    if (name,charAt(0).toLowerCase()=="w") {
+        intelligence =intelligenceLevels(5);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="x") {
+        intelligence =intelligenceLevels(6);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="y") {
+        intelligence =intelligenceLevels(1);
+    alert (intelligence);
+ }
+ else 
+
+    if (name,charAt(0).toLowerCase()=="z") {
+        intelligence =intelligenceLevels(2);
+    alert (intelligence);
+ }
+}
